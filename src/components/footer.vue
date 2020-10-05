5520593a0e73d8b3c574a49ac17c2072c3f4f314
Pablo Araneda Vargas <pabloav@MacBook-Pro-de-Pablo.local>
2020-10-05 18:51:20 -0300

    Primera subida

---
+++ src/components/footer.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <p> © Todos los derechos reservados  - Las Maravillas Secret Lodge - 2020 </p>
+    </div>  
+</template>
+
+
+<script>
+
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+
+
+</script>
+
+
+
+<style scoped>
+p.{
+
+font-size: 20px;
+
+}
+
+</style>
