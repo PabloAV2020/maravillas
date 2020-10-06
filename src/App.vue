83574307457afbcd7c260ba32d3e1755af65abc8
Pablo Araneda Vargas <pabloav@MacBook-Pro-de-Pablo.local>
2020-10-05 21:46:54 -0300

    footer agregado

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
  
   <Home msg="Welcome to Your Vue.js App"/>
+  <div>
+    <webapp-footer />
+    </div>
 </template>
 
 <script>
 import Home from './components/Home.vue'
+import footer from './components/footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Home
+  Home,
+    'webapp-footer':footer,
   }
 }
 </script>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class=myfooter>
     <p> © Todos los derechos reservados  - Las Maravillas Secret Lodge - 2020 </p>
     </div>  
 </template>
@@ -7,22 +7,19 @@
 
 <script>
 
-export default {
-  name: 'Home',
-  props: {
-    msg: String
-  }
-
-
 </script>
 
 
 
 <style scoped>
-p.{
 
-font-size: 20px;
+.myfooter{
+
+background-color: black;
+
+
 
 }
+
 
 </style>
